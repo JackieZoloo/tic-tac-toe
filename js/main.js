9c6f4b78d8d003e1b9038752ef6d09447bc5fad2
jackiezoloo <62490560+JackieZoloo@users.noreply.github.com>
2020-05-19 15:57:08 -0600

    do with the project

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 const playerTwo = document.getElementById("playerTwo");
 const playerOne = document.getElementById("playerOne");
 const reset = document.getElementById("reset");
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-    ];
+const winner = document.getElementById("winner");
+
+
+
 const squares = Array.from(document.querySelectorAll('.square'));
 
 
 let playerFirst = "o";
 let playerSecond = "x";
 let moves = 0;
+let divs = [
+    null, null, null,
+    null, null, null,
+    null, null, null
+]
 
 
 let player
@@ -29,32 +28,65 @@ playerOne.addEventListener("click", function(){
 
 reset.addEventListener("click", function(){
     squares.forEach(board => board.innerHTML = "");
+     divs = [
+        null, null, null,
+        null, null, null,
+        null, null, null
+    ]
 })
-const divs = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
+
 
 
 
 squares.forEach(function(square, index){
-    square.addEventListener('click', function(){
-            let numbers = square.id.split("");
-            let indexOne = Number(numbers[0]);
-            let indexTwo = Number(numbers[1]);
-            console.log(indexOne, indexTwo);
-        if(player === playerFirst) {
-            square.innerHTML = playerFirst;
-            player = playerSecond;
-            divs[indexOne][indexTwo] = 1;
-            console.log(divs);
-        } else if (player === playerSecond) {
-            square.innerHTML = playerSecond;
-            player = playerFirst;
-            divs[indexOne][indexTwo] = -1;
-            console.log(divs);
+    square.addEventListener('click', function() {
+        if(!square.innerHTML)  {
+            if(player === playerFirst)  {
+                square.innerHTML = playerFirst;
+                player = playerSecond;
+                divs[square.id]= 1;
+                checkWinner();
+                
+            } else if (player === playerSecond) {
+                square.innerHTML = playerSecond;
+                player = playerFirst;
+                divs[square.id] = -1;
+                console.log(divs);
+                checkWinner();
+            
         }
+        }  
+        
     })
   })
- 
+function checkWinner() {
+    winningCombos.forEach(function(array){
+        // console.log(array);
+        // console.log(divs);
+        if(divs[array[0]] !== null && divs[array[1]] !== null && divs[array[2]] !== null){
+            if(divs[array[0]] === divs[array[1]] && divs[array[1]] === divs[array[2]] ) {
+                if(player === playerFirst) {
+                    winner.innerHTML = "X wins";
+                } else {
+                    winner.innerHTML = "O wins";
+                }
+            }
+
+        }
+
+       
+    })
+
+    
+
+}
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
